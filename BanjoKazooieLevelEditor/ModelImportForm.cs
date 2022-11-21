39c4a5622e16093c5072349f6dacb6ce6e0798c3
Krien Linnenbank <krien.linnenbank@ziggo.nl>
2022-11-21 22:12:57 +0100

    Some cleanup done

--- BanjoKazooieLevelEditor/ModelImportForm.cs
+++ BanjoKazooieLevelEditor/ModelImportForm.cs
@@ -7118,7 +7118,7 @@ namespace BanjoKazooieLevelEditor
       this.pnlPreview.Name = "pnlPreview";
       this.pnlPreview.Size = new Size(190, 17);
       this.pnlPreview.TabIndex = 38;
-      this.colorPick_pbx.Image = Tool.ModelImportForm.colorPick_pbx;
+      //this.colorPick_pbx.Image = Tool.ModelImportForm.colorPick_pbx;
       this.colorPick_pbx.Location = new Point(6, 19);
       this.colorPick_pbx.Name = "colorPick_pbx";
       this.colorPick_pbx.Size = new Size(199, 80);
